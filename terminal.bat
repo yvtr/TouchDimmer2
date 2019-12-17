6701d8fde26eeb331ef602b481a59566db0bb407
unicod <yvtr@protonmail.com>
2019-12-17 08:28:32 +0100

    terminal bat updated

--- terminal.bat
+++ terminal.bat
@@ -2,4 +2,5 @@ echo %USERPROFILE%| C:\MinGW\msys\1.0\bin\tr "C\\" "c/" > temp.tmp
 <temp.tmp (set /p USERPROFILE=)
 del temp.tmp
 SET HOME=.
+SET PATH=%PATH%;C:\Program Files\Git\usr\bin
 call C:\MinGW\msys\1.0\msys.bat
